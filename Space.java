7f1e50c14fc068cfaa0af52f59608531e3d78ee0
kelechiu10 <34874074+kelechiu10@users.noreply.github.com>
2018-05-11 14:16:56 -0700

    Create Space.java
    still unfinished

---
+++ Space.java
@@ -0,0 +1,18 @@
+public abstract Space
+{
+  protected Event[] events;
+  //image
+  private Piece piece;
+  
+  public Space(Event[] e)
+  {
+    events = e;
+  }
+  
+  public void addPiece(Piece p)
+  {
+    piece = piece;
+  }
+ 
+  
+}
