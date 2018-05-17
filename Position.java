f4257a33f9b0c6f0bbb9fb7463e5b92d3ff4e2be
Matthew-Oh <39202781+Matthew-Oh@users.noreply.github.com>
2018-05-17 12:25:03 -0700

    Create Position.java
    reinventing the wheel

---
+++ Position.java
@@ -0,0 +1,40 @@
+/**
+ * Position Object
+ * because for some reason we can't just use point2d
+ * @author Matthew Oh
+ * @version 5/17/18
+ */
+ public class Position
+ {
+    private int xPos;
+    private int yPos;
+    
+    /**
+     * Constructor for Position
+     * @param x x position
+     * @param y y position
+     */
+    public Position(int x, int y)
+    {
+        xPos = x;
+        yPos = y;
+    }
+    
+    /**
+     * Returns the x position
+     * @return the x position
+     */
+    public getX()
+    {
+        return xPos;
+    }
+    
+     /**
+     * Returns the y position
+     * @return the y position
+     */
+    public getY()
+    {
+        return yPos;
+    }
+ }
