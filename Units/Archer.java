450fa9a26d1a3f29173f844e0e3f2ecefd50626b
Matthew-Oh <39202781+Matthew-Oh@users.noreply.github.com>
2018-05-22 14:11:23 -0700

    Create Archer.java

---
+++ Units/Archer.java
@@ -0,0 +1,32 @@
+/**
+ * Archer Unit Class
+ * Attack: 12
+ * Defence: 10
+ * Health: 45
+ * Movement: 5
+ * Abilities:
+ * -#1 Shoot: Damages unit at a distance
+ *
+ * @author Matthew Oh
+ * @version 5/22/18
+ */
+public class Archer extends Unit
+{
+    /**
+     * Constructor for Archer
+     */
+    public Archer()
+    {
+        super(12,10,45,5);
+    }
+    
+    /**
+     * Gets Archer's ability
+     * @param target target of the ability
+     * @param num number of ability to be used
+     */
+    public Action getAbility(Position target, int num)
+    {
+        return new Action(target, 15, "attack");
+    }
+}
