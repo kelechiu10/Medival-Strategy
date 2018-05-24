1d0ec71d0b5d9217d6b0f6956972ad7e48158ba2
Matthew-Oh <39202781+Matthew-Oh@users.noreply.github.com>
2018-05-24 13:13:59 -0700

    Create Grass.java

---
+++ Grass.java
@@ -0,0 +1,12 @@
+/**
+ * Grass Space Class
+ * @author Matthew Oh
+ * @version 5/24/18
+ */
+ public class Grass extends Space
+ {
+    public Grass()
+    {
+        super(true, "grass");
+    }
+ }
