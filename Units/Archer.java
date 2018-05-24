37a5c9d34aff3bf2a9ccbce3edee79b4364e5ae4
Matthew-Oh <39202781+Matthew-Oh@users.noreply.github.com>
2018-05-24 12:39:08 -0700

    Update Archer.java

--- Units/Archer.java
+++ Units/Archer.java
@@ -8,7 +8,7 @@
  * -#1 Shoot: Damages unit at a distance
  *
  * @author Matthew Oh
- * @version 5/22/18
+ * @version 5/24/18
  */
 public class Archer extends Unit
 {
@@ -23,11 +23,10 @@ public class Archer extends Unit
     /**
      * Gets Archer's ability
      * @param target target of the ability
-     * @param num number of ability to be used
      */
-    public Action getAbility(Position target, int num)
+    public Action getAbility(Position target)
     {
-        return new Action(target, power, "attack");
+        return new Action(target, current, "attack");
     }
     
     /**
@@ -37,5 +36,14 @@ public class Archer extends Unit
     public double getRange()
     {
         return 8.0;
+    }
+    
+    /**
+     * Gets the power of the Archers's ability
+     * @return the power of the Archer's ability
+     */
+    public int getAbilityPower()
+    {
+        return 13;
     }
 }
