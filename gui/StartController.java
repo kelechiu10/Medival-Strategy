55e4fd1e126dc4453acd4967a7ff69edadb7dde7
Kelechi Uhegbu <34874074+kelechiu10@users.noreply.github.com>
2018-05-29 07:51:05 -0700

    Add files via upload

--- gui/StartController.java
+++ gui/StartController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -14,25 +14,21 @@ import javafx.stage.Stage;
 public class StartController {
 	public AnchorPane rootPane;
 	public Pane pane;
-	public Label label;
 	public Button button;
+	public TextArea text;
+	
 	public void startGame()
 	{
-		Stage window;
 		Stage oldStage;
-		try {
+		try 
+		{
 			oldStage = (Stage) button.getScene().getWindow();
-			oldStage.close();
-			//Pane newPane = FXMLLoader.load(getClass().getResource("/gui/GameScreen.fxml"));
-			//rootPane.getChildren().setAll(pane);
 			Parent root = FXMLLoader.load(getClass().getResource("/gui/GameScreen.fxml"));
-			window = new Stage();
-			window.setScene(new Scene(root));
-			window.setResizable(false);
-			window.show();
+			oldStage.setScene(new Scene(root));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 }
